eec247517cfb804d9b632e8942f71599ea69b2ad
Dzik-Guari <francois.carniere.cd@outlook.com>
2020-11-14 00:55:02 +0100

    ajout des sécurités

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WebRoute from './components/WebRoute';
 import NavBar from "./components/NavBar";
--- client/src/components/addUser.js
+++ client/src/components/addUser.js
@@ -49,7 +49,7 @@ export class AddUser extends Component {
 
     toggleDisplay = () => {
         this.elmt = document.getElementById('isShow');
-        if(document.getElementById('isStudent').checked == true) this.elmt.style.display = "";
+        if(document.getElementById('isStudent').checked === true) this.elmt.style.display = "";
         else this.elmt.style.display = "none";
     }
 
@@ -62,30 +62,124 @@ export class AddUser extends Component {
 
     onClick = () =>{
         this.isStudent = 0;
-        if(document.getElementById('isStudent').checked == true){
+        if(document.getElementById('isStudent').checked === true){
             this.isStudent = 1;
         }
-        Axios.post(`http://localhost:5000/users`, {   
-            user_id: null,
-            class_id: null,
-            firstname: this.state.firstname,
-            lastname: this.state.lastname,
-            emailAddress: this.state.emailAddress,
-            password: this.state.password,
-            address: this.state.address,
-            inscriptiondate: null,
-            student: this.isStudent,
-            phoneNumberTutor1: this.state.phoneNumberTutor1,
-            phoneNumberTutor2: this.state.phoneNumberTutor2,
-            emailTutor1: this.state.emailTutor1,
-            emailTutor2: this.state.emailTutor2
-        })
-        .then(function (res) {
-            console.log(res);
-        })
-        .catch(function (err){
-            console.log(err)
-        }) 
+
+        const Security = require("./Security");
+        const security = new Security();
+
+        let v_lastname = this.state.lastname;
+        let v_firstname = this.state.firstname;
+        let v_address = this.state.address;
+        let v_emailAddress = this.state.emailAddress;
+        let v_password = this.state.password;
+        let v_student = this.isStudent;
+        let v_phoneNumberTutor1 = this.state.phoneNumberTutor1;
+        let v_phoneNumberTutor2 = this.state.phoneNumberTutor2;
+        let v_emailTutor1 = this.state.emailTutor1;
+        let v_emailTutor2 = this.state.emailTutor2;
+
+        try
+        {
+            if (v_student === 1 || v_student === true || v_student === "1")
+            {
+                try
+                {
+                    v_phoneNumberTutor1 = v_phoneNumberTutor1.trim();
+                    v_phoneNumberTutor2 = v_phoneNumberTutor2.trim();
+                    v_emailTutor1 = v_emailTutor1.trim();
+                    v_emailTutor2 = v_emailTutor2.trim();
+                }
+                catch (error)
+                {
+                    console.log("trying to trim nonexistent data");
+                }
+            }
+
+            v_firstname = v_firstname.trim();
+            v_lastname = v_lastname.trim();
+            v_address = v_address.trim();
+            v_emailAddress = v_emailAddress.trim();
+            v_password = v_password.trim();
+
+            let testOk = false;
+
+            if (security.firstNameVerification(v_firstname) === false)
+            {
+                if (security.lastNameVerification(v_lastname) === false)
+                {
+                    if (security.addressVerification(v_address) === false)
+                    {
+                        if (security.emailVerification(v_emailAddress) === false)
+                        {
+                            if (security.passwordVerification(v_password) === false)
+                            {
+                                if (security.studentVerification(v_student) === false)
+                                {
+                                    if (v_student === 1 || v_student === true || v_student === "1")
+                                    {
+                                        if (security.phoneVerification(v_phoneNumberTutor1) === false)
+                                        {
+                                            if (security.phoneVerification(v_phoneNumberTutor2) === false)
+                                            {
+                                                if (security.emailVerification(v_emailTutor1) === false)
+                                                {
+                                                    if (security.emailVerification(v_emailTutor2) === false)
+                                                    {
+                                                        testOk = true;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        v_emailTutor1 = "undefined";
+                                        v_emailTutor2 = "undefined";
+                                        v_phoneNumberTutor1 = "undefined";
+                                        v_phoneNumberTutor2 = "undefined";
+                                        testOk = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (testOk === true)
+            {
+                Axios.post(`http://localhost:5000/users`, {   
+                    user_id: null,
+                    class_id: null,
+                    firstname: v_firstname,
+                    lastname: v_lastname,
+                    emailAddress: v_emailAddress,
+                    password: v_password,
+                    address: v_address,
+                    inscriptiondate: null,
+                    student: v_student,
+                    phoneNumberTutor1: v_phoneNumberTutor1,
+                    phoneNumberTutor2: v_phoneNumberTutor2,
+                    emailTutor1: v_emailTutor1,
+                    emailTutor2: v_emailTutor2
+                })
+                .then(function (res) {
+                    console.log(res);
+                })
+                .catch(function (err){
+                    console.log(err)
+                }) 
+            }
+            else
+            {
+                console.log("Bad character detected aborting the query, please try again!");
+            }
+        }
+        catch (err)
+        {
+            console.error("Error while creating an user" + err.message);
+        }
     }
     
 }
